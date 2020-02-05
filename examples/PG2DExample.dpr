9fc68ffe7b6d2179d1ba2d97298444eb2c953285
ido@pitaron.info <sharonido>
2020-02-05 21:38:01 +0200

    comments

--- examples/PG2DExample.dpr
+++ examples/PG2DExample.dpr
@@ -1,10 +1,24 @@
+{* copyright (c) 2020 I. Sharon Ltd.
+ *
+ * This file is part of GStreamer 2 Delphi bridge (G2D).
+ *
+ * G2D is free software; You can redistribute it and modify it. It is licensed
+ * under the GNU Lesser General Public License as published by the Free Software
+ * Foundation. Either version 2.1 of the License, or any later version.
+
+for info on G2D goto
+https://github.com/sharonido/Delphi_GStreamer
+}
+
 program PG2DExample;
 
 {$APPTYPE CONSOLE}
 
 uses
   System.SysUtils,
-  G2D;   // in 'G2D.pas';
+  G2D in '..\Delphi\G2D.pas',
+  G2DCallDll in '..\Delphi\G2DCallDll.pas';
+
 {---  readme.txt ---------------------------------------------------------------
 This program launches a gstreamer pipeline.
 The pipeline is constructed just with two standard known plugins. a source (src) linked to a sink.
